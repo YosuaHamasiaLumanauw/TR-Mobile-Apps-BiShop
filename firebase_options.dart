adb18c5bb186f6110d227676e8dd04fefcab59ea
yosua hamasia lumanauw <yosua.lumanauw@binus.ac.id>
2024-10-08 11:38:42 +0700

    Add files via upload

---
+++ firebase_options.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web - '
+        'you can reconfigure this by running the FlutterFire CLI again.',
+      );
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for ios - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.macOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyBCbuqg--adaMy4GqYAVIM43nwEsNMaseI',
+    appId: '1:776378446210:android:3573edeb985f9db7f69f9b',
+    messagingSenderId: '776378446210',
+    projectId: 'tr-olx',
+    storageBucket: 'tr-olx.appspot.com',
+  );
+}
---
+++ main.dart
@@ -0,0 +1,97 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:Bishop/screens/createpost.dart';
+import 'package:Bishop/screens/home.dart';
+import 'package:Bishop/screens/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:Bishop/screens/profil.dart';
+import 'firebase_options.dart';
+
+final _auth = FirebaseAuth.instance;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  bool isUserLoggedIn = FirebaseAuth.instance.currentUser != null;
+  runApp(MainApp(
+    initialLoginStatus: isUserLoggedIn,
+  ));
+}
+
+class MainApp extends StatefulWidget {
+  final bool initialLoginStatus;
+  const MainApp({super.key, required this.initialLoginStatus});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  late bool isLogin;
+
+  @override
+  void initState() {
+    super.initState();
+    isLogin = widget.initialLoginStatus;
+  }
+
+  void updateLoginStatus(bool isLogin) {
+    setState(() {
+      this.isLogin = isLogin;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Bishop",
+      theme: ThemeData(
+        textTheme: GoogleFonts.latoTextTheme(),
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(),
+                  SizedBox(height: 20),
+                  Text('Loading...'),
+                ],
+              ),
+            );
+          }
+          if (snapshot.hasData) {
+            return Home(
+              login: isLogin,
+              onLoginStatusChanged: updateLoginStatus,
+            );
+          } else {
+            return Login(
+              onLoginStatusChanged: updateLoginStatus,
+            );
+          }
+        },
+      ),
+      routes: {
+        '/login': (context) => Login(
+              onLoginStatusChanged: updateLoginStatus,
+            ),
+        '/home': (context) => Home(
+              login: isLogin,
+              onLoginStatusChanged: updateLoginStatus,
+            ),
+        '/profile': (context) => Profile(
+              onLoginStatusChanged: updateLoginStatus,
+            ),
+        '/create_post': (context) => const CreatePost(),
+      },
+    );
+  }
+}
